4a5913a65ea703c51cae9791a47ab8ec2186fdd4
Stanislav Kaisin <8050361@gmail.com>
2019-10-22 20:45:40 +0300

    route post planning tasks

--- controllers/defaultTasks.controller.js
+++ controllers/defaultTasks.controller.js
@@ -3,99 +3,118 @@ const Joi = require('@hapi/joi');
 
 const getDefaultTasks = (req, res) => {
   const userId = req.user.id;
-  console.log('userId :', userId);
-	DefaultTasks.aggregate([
-    {$match: {}},
-    {}
-  ])
-		.then(result => res.json({result}))
-		.catch(err => {
-			throw new Error(err);
-		});
+  DefaultTasks.find()
+    .then(result => res.json({
+      result
+    }))
+    .catch(err => {
+      throw new Error(err);
+    });
 };
 
 const getDefaultTask = (req, res) => {
-	const defaultTaskId = req.params.defaultTaskId;
+  const defaultTaskId = req.params.defaultTaskId;
 
-	DefaultTasks.findById(defaultTaskId)
-		.then(result => res.json({result}))
-		.catch(err => {
-			throw new Error(err);
-		});
+  DefaultTasks.findById(defaultTaskId)
+    .then(result => res.json({
+      result
+    }))
+    .catch(err => {
+      throw new Error(err);
+    });
 };
 
 const createDefaultTask = (req, res, next) => {
-	const taskData = req.body;
-	const schema = Joi.object({
-		cardTitle: Joi.string().required(),
-		imageUrl: Joi.string
-			.uri({
-				scheme: ['https']
-			})
-			.required()
-	});
+  const taskData = req.body;
 
-	const {error, value} = schema.validate(taskData);
+  const schema = Joi.object({
+    cardTitle: Joi.string().required(),
+    imageUrl: Joi.string
+      .uri({
+        scheme: ['https']
+      })
+      .required()
+  });
 
-	if (error) {
-		return next(error);
-	}
+  const {
+    error,
+    value
+  } = schema.validate(taskData);
 
-	const newDefaultTask = new DefaultTasks(value);
+  if (error) {
+    return next(error);
+  }
 
-	newDefaultTask
-		.save()
-		.then(result => res.json({result}))
-		.catch(err => {
-			throw new Error(err);
-		});
+  const newDefaultTask = new DefaultTasks(value);
+
+  newDefaultTask
+    .save()
+    .then(result => res.json({
+      result
+    }))
+    .catch(err => {
+      throw new Error(err);
+    });
 };
 
 const updateDefaultTask = (req, res) => {
-	const defaultTaskId = req.params.defaultTaskId;
-	const taskForUpdate = req.body;
+  const defaultTaskId = req.params.defaultTaskId;
+  const taskForUpdate = req.body;
 
-	const schema = Joi.object({
-		cardTitle: Joi.string().required(),
-		imageUrl: Joi.string
-			.uri({
-				scheme: ['https']
-			})
-			.required()
-	});
-	const {error, value} = schema.validate(taskForUpdate);
-	if (error) {
-		return next(error);
-	}
+  const schema = Joi.object({
+    cardTitle: Joi.string().required(),
+    imageUrl: Joi.string
+      .uri({
+        scheme: ['https']
+      })
+      .required()
+  });
+  const {
+    error,
+    value
+  } = schema.validate(taskForUpdate);
+  if (error) {
+    return next(error);
+  }
 
-	DefaultTasks.findByIdAndUpdate({_id: defaultTaskId}, {$set: value})
-		.then(result => {
-			if (!result) {
-				return res
-					.status(404)
-					.json({message: `Default task with id ${defaultTaskId} not found`});
-			}
-			res.json({result});
-		})
-		.catch(err => {
-			throw new Error(err);
-		});
+  DefaultTasks.findByIdAndUpdate({
+      _id: defaultTaskId
+    }, {
+      $set: value
+    })
+    .then(result => {
+      if (!result) {
+        return res
+          .status(404)
+          .json({
+            message: `Default task with id ${defaultTaskId} not found`
+          });
+      }
+      res.json({
+        result
+      });
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
 };
 
 const deleteDefaultTask = (req, res) => {
-	const defaultTaskId = req.params.defaultTaskId;
+  const defaultTaskId = req.params.defaultTaskId;
 
-	DefaultTasks.findByIdAndDelete(defaultTaskId)
-		.then(result => res.json({result}))
-		.catch(err => {
-			throw new Error(err);
-		});
+  DefaultTasks.findByIdAndDelete(defaultTaskId)
+    .then(result => res.json({
+      result
+    }))
+    .catch(err => {
+      throw new Error(err);
+    });
 };
 
 module.exports = {
-	createDefaultTask,
-	getDefaultTasks,
-	getDefaultTask,
-	updateDefaultTask,
-	deleteDefaultTask
+  createDefaultTask,
+  getDefaultTasks,
+  getDefaultTask,
+  updateDefaultTask,
+  deleteDefaultTask,
 };
--- controllers/taskPlanning.controller.js
+++ controllers/taskPlanning.controller.js
@@ -1,13 +1,107 @@
 const PlanningTasks = require('../models/planningTasks.model');
+const Tasks = require('../models/tasks.model.js');
+const moment = require('moment');
+const ObjectId = require('mongoose').Types.ObjectId;
 
-exports.getTasks = (req, res) => {
-	const userId = req.user.id;
-	PlanningTasks.find({userId})
-		.select({__v: 0})
-		.then(result => {
-			res.json({status: 'OK', planningTasks: result });
-		})
-		.catch(err => {
-			throw new Error(err);
-		});
+const getTasks = (req, res) => {
+  const userId = req.user.id;
+  const today = moment().locale('uk', {
+    week: {
+      dow: 1 // Monday is the first day of the week
+    }
+  });
+
+  const fromDate = today
+    .set({
+      hour: 3,
+      minute: 0,
+      second: 0,
+      millisecond: 0
+    })
+    .weekday(0)
+    .toISOString();
+  const toDate = today
+    .set({
+      hour: 23,
+      minute: 59,
+      second: 59
+    })
+    .weekday(6)
+    .toISOString();
+
+  PlanningTasks.find({
+      userId
+    })
+    .select({
+      __v: 0
+    })
+    .select({
+      userId: 0
+    })
+    .then(result => {
+      PlanningTasks.aggregate([{
+          $match: {
+            userId: ObjectId(userId)
+          }
+        },
+        {
+          $match: {
+            date: {
+              $gte: new Date(fromDate),
+              $lte: new Date(toDate)
+            }
+          }
+        }
+      ]).then(aggrResult => {
+        res.json({
+          status: 'OK',
+          weekRange: {
+            fromDate: fromDate,
+            toDate: toDate
+          },
+          aggrResult: aggrResult,
+          planningTasks: result
+        });
+      })
+
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+};
+
+const postTasks = (req, res) => {
+  // console.log('req.body.tasks', req.body.tasks)
+  const tasksFromReq = req.body.tasks;
+  const userId = req.user._id;
+  const taskDaysArr = [];
+  tasksFromReq.map(({
+    taskId,
+    selectedDays
+  }) => {
+    selectedDays.map((day) => {
+      return taskDaysArr.push({
+        userId,
+        task: taskId,
+        date: moment(day, ["MM-DD-YYYY", "DD-MM", "DD-MM-YYYY"])
+      })
+    })
+  })
+  // console.log('taskDaysArr', taskDaysArr)
+  Tasks.insertMany(taskDaysArr).then(
+    (result) => {
+      res.json({
+        status: 'OK',
+        planningTasks: result
+      });
+    }
+
+  ).catch(err => {
+    throw new Error(err);
+  })
+  // console.log('taskDaysArr', taskDaysArr)
+}
+module.exports = {
+  postTasks,
+  getTasks,
 };
