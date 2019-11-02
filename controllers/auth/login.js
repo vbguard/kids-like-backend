d01d4600913388923d42dad9f6f498ff4ad8d063
Stanislav Kaisin <8050361@gmail.com>
2019-11-02 17:32:10 +0200

    added update password route

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -4,77 +4,74 @@ const Joi = require('@hapi/joi');
 
 // Login User and get him Token for access to some route action
 const userLogin = (req, res, next) => {
-	const schema = Joi.object()
-		.keys({
-			email: Joi.string()
-				.regex(
-					/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-				)
-				.required(),
-			// nickname: Joi.string()
-			// 	.required()
-			// 	.min(5)
-			// 	.max(15),
-			password: Joi.string()
-				.required()
-				.min(6)
-				.max(16)
-		})
-		.options({
-			presence: 'required',
-			stripUnknown: true,
-			abortEarly: false
-		});
+  const schema = Joi.object()
+    .keys({
+      email: Joi.string()
+        .regex(
+          /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        )
+        .required(),
+      // nickname: Joi.string()
+      // 	.required()
+      // 	.min(5)
+      // 	.max(15),
+      password: Joi.string()
+        .required()
+        .min(6)
+        .max(16)
+    })
+    .options({
+      presence: 'required',
+      stripUnknown: true,
+      abortEarly: false
+    });
 
-	const result = schema.validate(req.body);
+  const result = schema.validate(req.body);
 
-	if (result.error) {
-		return next(result.error);
-	}
+  if (result.error) {
+    return next(result.error);
+  }
 
-	const sendResponse = user => {
-		res.json({
-			status: 'success',
-			user
-		});
-	};
+  const sendResponse = user => {
+    res.json({
+      status: 'success',
+      user
+    });
+  };
 
-	const sendError = error => {
-		const errMessage = error.message || 'must handle this error on login';
-		res.status(400).json({
-			status: 'error',
-			error: errMessage
-		});
-	};
+  const sendError = error => {
+    const errMessage = error.message || 'must handle this error on login';
+    res.status(400).json({
+      status: 'error',
+      error: errMessage
+    });
+  };
 
-	passport.authenticate(
-		'local',
-		{
-			session: false
-		},
-		(err, user, info) => {
-			if (err || !user) {
-				const infoMessage = info ? info : {message: 'Login failed'};
-				sendError(infoMessage);
-				return;
-			}
-			req.login(
-				user,
-				{
-					session: false
-				},
-				err => {
-					if (err) {
-						res.send(err);
+  passport.authenticate(
+    'local', {
+      session: false
+    },
+    (err, user, info) => {
+      if (err || !user) {
+        const infoMessage = info ? info : {
+          message: 'Login failed'
+        };
+        sendError(infoMessage);
+        return;
+      }
+      req.login(
+        user, {
+          session: false
+        },
+        err => {
+          if (err) {
+            res.send(err);
           }
-          // const token = user.getJwt();
-          // user.token = token;
-          // console.log(token);
-					sendResponse(user);
-				}
-			);
-		}
-	)(req, res);
+          sendResponse(user);
+        }
+      );
+    }
+  )(req, res);
 };
 
 module.exports = userLogin;
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -7,70 +7,65 @@ const Joi = require('@hapi/joi');
 
 // Register New User and Check this email have in DB
 const userSignup = (req, res, next) => {
-	const schema = Joi.object()
-		.keys({
-			email: Joi.string()
-				.regex(
-					/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-				)
-				.required(),
-			password: Joi.string()
-				.min(5)
-				.max(12)
-				.required(),
-			nickname: Joi.string()
-				.min(5)
-				.max(15)
-				.required()
-		})
-		.options({
-			stripUnknown: true,
-			abortEarly: false
-		});
+  const schema = Joi.object()
+    .keys({
+      email: Joi.string()
+        .regex(
+          /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        )
+        .required(),
+      password: Joi.string()
+        .min(5)
+        .max(12)
+        .required(),
+      nickname: Joi.string()
+        .min(5)
+        .max(15)
+        .required()
+    })
+    .options({
+      stripUnknown: true,
+      abortEarly: false
+    });
 
-	const result = schema.validate(req.body);
+  const result = schema.validate(req.body);
 
-	if (result.error) {
-		return next(result.error);
-	}
-	// console.log('result', result);
+  if (result.error) {
+    return next(result.error);
+  }
 
-	// if (result.error) {
-	// 	throw new ValidationError(result.error.message);
-	// }
+  const sendError = error => {
+    const errMessage =
+      error.message || 'must handle this error on registration';
+    res.json({
+      status: 'error',
+      error: errMessage
+    });
+  };
 
-	const sendError = error => {
-		const errMessage =
-			error.message || 'must handle this error on registration';
-		res.json({
-			status: 'error',
-			error: errMessage
-		});
-	};
-
-	const newUser = new User(result.value);
-	newUser
-		.save()
-		.then(() => {
-			DefaultTasks.find({})
-				.then(defaultTasks => {
-					console.log('defaultTasks :', defaultTasks);
-					const defaultTasksWithUserId = defaultTasks.map(task => ({
-						userId: newUser._id,
-						cardTitle: task.cardTitle,
-						imageUrl: task.imageUrl
-					}));
-					PlanningTasks.insertMany(defaultTasksWithUserId)
-						.then(savingPlanTasks => {
-							if (savingPlanTasks) {
-								login(req, res);
-							}
-						})
-						.catch(sendError);
-				})
-				.catch(sendError);
-		})
-		.catch(sendError);
+  const newUser = new User(result.value);
+  newUser
+    .save()
+    .then(() => {
+      DefaultTasks.find({})
+        .then(defaultTasks => {
+          // console.log('defaultTasks :', defaultTasks);
+          const defaultTasksWithUserId = defaultTasks.map(task => ({
+            userId: newUser._id,
+            cardTitle: task.cardTitle,
+            imageUrl: task.imageUrl
+          }));
+          PlanningTasks.insertMany(defaultTasksWithUserId)
+            .then(savingPlanTasks => {
+              if (savingPlanTasks) {
+                login(req, res);
+              }
+            })
+            .catch(sendError);
+        })
+        .catch(sendError);
+    })
+    .catch(sendError);
 };
 
 module.exports = userSignup;
