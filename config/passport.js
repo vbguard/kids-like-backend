8e938e87702ee3ae7183c5e6f5d498ed7a7cd518
Viktor Bulvarenko <silken.ua@gmail.com>
2019-12-14 02:47:29 +0200

    gdsgds

--- config/passport.js
+++ config/passport.js
@@ -92,7 +92,7 @@ module.exports = function(passport) {
             return done(null, { ...user, token });
           }
           if (!user) {
-            const newUser = await new User({
+            const newUser = await new Users({
               googleId: profile._json.sub,
               name: { fullName: profile._json.name },
               photo: profile._json.picture,
